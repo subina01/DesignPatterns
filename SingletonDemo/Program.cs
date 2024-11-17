f6084ab1b94603c9d6696e3e9c21bcaaa60088e1
Subina Dhakal <subiakal1212@gmail.com>
2024-11-17 13:39:27 +0545

    Singleton

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            PrintEmpDetails();
+            PrintStuDetails();
+
+        }
+
+        private static void PrintStuDetails()
+        {
+            Singleton FromStudent = Singleton.GetInstance;
+            FromStudent.PrintDetails("From Student");
+        }
+
+        private static void PrintEmpDetails()
+        {
+            Singleton FormEmployee = Singleton.GetInstance;
+            FormEmployee.PrintDetails("From Employee");
+        }
+    }
+}
---
+++ TodoApp/SingletonImplementation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public sealed class SingletonImplementation
+    {
+        private static readonly SingletonImplementation instance = new SingletonImplementation();
+
+        private readonly List<string> tasks = new List<string>();
+
+        private SingletonImplementation() {
+        
+            tasks = new List<string>();
+        
+        }
+
+        public static SingletonImplementation Instance { get { return instance; } }
+
+        public void AddTask(string task) {
+        tasks.Add(task);
+            Console.WriteLine("Task Added!");
+        
+        }
+
+
+        public void DisplayTask()
+        {
+            if (tasks.Count == 0)
+            {
+                Console.WriteLine("No task available");
+            }
+            else {
+                Console.WriteLine("To do List");
+                for (int i = 0; i < tasks.Count; i++) {
+                Console.WriteLine($"{i +1}. {tasks[i]}");
+                
+                }
+            
+            }
+        }
+    }
+}
