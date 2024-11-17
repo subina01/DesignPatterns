4824e14ea9d8fb0e62a3d889a78d5640c396ee6a
Subina Dhakal <subiakal1212@gmail.com>
2024-11-17 15:59:59 +0545

    Factory Design Pattern

---
+++ FactoryDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+namespace FactoryDemo
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            Factory factory = new SubFactory();//creating object without exposing creation logic to clients
+            ProductionFactory product = factory.ProductionProcess("Product3");
+
+            product.ProducedGood();
+        
+        
+        }
+    }
+}
